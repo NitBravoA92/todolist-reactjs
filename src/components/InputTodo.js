c2dbf784e9cc9f3dbf26a0ae89889e566c3f048d
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 13:59:58 -0400

    Create the component InputTodo

---
+++ src/components/InputTodo.js
@@ -0,0 +1,17 @@
+const InputTodo = () => {
+  const newTask = (e) => {
+    e.preventDefault();
+    console.log(e.target.inputDescription);
+  }
+
+  return (
+    <form id="form-input-todo" onSubmit={newTask}>
+      <div id="container-input-todo">
+        <input type="text" placeholder="Add todo..." id="inputDescription" />
+        <button type="submit" id="submit-new-task">+</button>
+      </div>
+  </form>
+  );
+}
+
+export default InputTodo;

f347a4f7a4ca21f82023b18ae67478889f31a19a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:05:37 -0400

    Create a custome hook useTodoList

---
+++ src/hooks/useTodoList.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const useTodoList = (initialValue) => {
+  const setLocalStorage = (todoList) => {
+    localStorage.setItem('todolist-app', JSON.stringify(todoList));
+  }
+  
+  const getFromLocalStorage = () => {
+    const getTodoList = localStorage.getItem('todolist-app');
+    const dataParse = getTodoList ? JSON.parse(getTodoList) : [];
+    return dataParse;
+  }
+  
+  const [todolist, setTodolist] = useState(initialValue);
+  
+  useEffect(() => {
+    setTodolist(getFromLocalStorage()); 
+  }, []);
+
+  const saveTask = (title) => {
+    const newId = todolist.length + 1;
+    const tasksList = [...todolist];
+    tasksList.push({
+        id: newId,
+        title: title,
+        completed: false,
+    });
+    setLocalStorage(tasksList);
+    setTodolist(getFromLocalStorage());
+  }
+
+  const updateTaskTitle = (id, title) => {
+    const tasksList = [...todolist];
+    tasksList.forEach((task, i) => {
+      if(task.id === id) {
+        tasksList[i].title = title
+      }
+    });
+    setLocalStorage(tasksList);
+    setTodolist(getFromLocalStorage());
+  }
+
+  return [
+    todolist,
+    saveTask,
+    updateTaskTitle
+  ]
+}
+
+export default useTodoList;

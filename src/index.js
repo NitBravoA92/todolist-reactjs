be6d5bfb19c0b839275044eed420f3fec19b519b
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:04:36 -0400

    Refactor the components inside the render function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TodoApp from './TodoApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>
 );
 

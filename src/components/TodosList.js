b9ccc9bdf96a38cf59f500568386578c64cde204
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:08:33 -0400

    Import the TodoItem component

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,9 +1,11 @@
-const TodosList = () => (
-  <div id="todo-list">
-    <ul id="list-items-container">
-    
-    </ul>
-  </div>
+import TodoItem from './TodoItem';
+
+const TodosList = ({ tasklist }) => (
+  <ul id="todos-items-container">
+  {
+    tasklist.map((item) => <TodoItem key={item?.id} title={item?.title} />)
+  }
+  </ul>
 );
 
 export default TodosList;

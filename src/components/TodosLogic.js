b4cb4b6009d4bfa2d3bf0c6863509dc186a8b6bc
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:07:07 -0400

    Initialize the state and Import the InputTodo and TodosList components

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,7 +1,17 @@
-const TodosLogic = () => (
-  <section id="todos-logic">
+import useTodoList from '../hooks/useTodoList';
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
 
-  </section>
-)
+const TodosLogic = () => {
+
+  const [todolist, saveTask] = useTodoList([]);
+
+  return (
+    <section id="todos-logic">
+      <InputTodo addTaskToState={saveTask} />
+      <TodosList tasklist={todolist} />
+    </section>
+  ) 
+}
 
 export default TodosLogic;

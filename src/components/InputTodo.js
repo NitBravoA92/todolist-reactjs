1187e9daee5b1e86c597f0396fa7591f4042b262
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:09:20 -0400

    Add the logic to create a new task

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,14 +1,22 @@
-const InputTodo = () => {
+import { FaCirclePlus } from "react-icons/fa6";
+
+const InputTodo = ({ addTaskToState }) => {
+
   const newTask = (e) => {
     e.preventDefault();
-    console.log(e.target.inputDescription);
+    const form = e.target;
+    const taskTitle = form.inputDescription.value;
+    if(taskTitle.length > 0) {
+      addTaskToState(taskTitle);
+      form.reset();
+    }
   }
 
   return (
     <form id="form-input-todo" onSubmit={newTask}>
       <div id="container-input-todo">
         <input type="text" placeholder="Add todo..." id="inputDescription" />
-        <button type="submit" id="submit-new-task">+</button>
+        <button type="submit" id="submit-new-task"><FaCirclePlus /></button>
       </div>
     </form>
   );

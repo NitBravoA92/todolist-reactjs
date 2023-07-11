66fd0586493c0bbd8005396ca429ed6f54750b4a
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:10:10 -0400

    Add the props and icons to the component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,18 @@
-const TodoItem = ({ description, updateTask, deleteTask }) => {
+import { AiFillEdit } from "react-icons/ai";
+import { FaTrash } from "react-icons/fa6";
+
+const TodoItem = ({ title }) => { // updateTask, deleteTask
   return (
     <li className="todo-item">
       <input type="checkbox" className="taskStatus" />
-      <h3 id="taskDescription">{description}</h3>
+      <h3 className="taskTitle">{title}</h3>
       <div className="actionButtons">
-        <button type="button" className="updateTask">icon-upd</button>
-        <button type="button" className="deleteTask">icon-del</button>
+        <button type="button" className="updateTask">
+          <AiFillEdit />
+        </button>
+        <button type="button" className="deleteTask">
+          <FaTrash />
+        </button>
       </div>
     </li>
   )

70ad86a503869560558879c10cc2384922067062
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-10 20:10:34 -0400

    Import the Header and TodosLogic components with their data

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,9 +1,12 @@
+import Header from './components/Header';
+import TodosLogic from './components/TodosLogic';
 import './App.css';
 
 function TodoApp() {
   return (
     <div className="App">
-
+      <Header title="Todos" subtitle="Items will persist in the browser local storage" />
+      <TodosLogic />
     </div>
   );
 }

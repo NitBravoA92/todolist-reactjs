e89df47f2702f646d390c6e5253102987cf81af9
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-11 23:16:45 -0400

    Remove propTypes validations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,8 @@
-import PropTypes from 'prop-types';
-
 const Header = () => (
   <section id="header">
     <h1>Todos</h1>
     <h2>Items will persist in the browser local storage</h2>
   </section>
 );
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-};
 
 export default Header;
